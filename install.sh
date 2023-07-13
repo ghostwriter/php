b08668004c2895bf512d05aabac39eb4932a73df
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-13 17:05:44 -0500

    Update install.sh
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- install.sh
+++ install.sh
@@ -1,2 +1,34 @@
 #! bin/sh -e
-install-php-extensions apcu bcmath gd imagick intl memcached mongodb opcache pcntl pdo pdo_mysql pdo_pgsql pdo_sqlite redis soap sockets uuid zip
+
+install-php-extensions apcu;
+install-php-extensions bcmath;
+install-php-extensions bz2;
+install-php-extensions curl;
+install-php-extensions dom;
+install-php-extensions gd;
+install-php-extensions gmp;
+# PHP 8.3 not supported yet
+if [ $(php -r "echo version_compare(PHP_VERSION, '8.2.999', '<');") = 1 ]; then
+    install-php-extensions imagick;    
+fi
+install-php-extensions intl;
+install-php-extensions mbstring;
+install-php-extensions memcached;
+install-php-extensions mongodb;
+install-php-extensions opcache;
+install-php-extensions pcntl;
+install-php-extensions pdo_mysql;
+install-php-extensions pdo_pgsql;
+install-php-extensions pdo_sqlite;
+install-php-extensions pdo;
+install-php-extensions redis;
+install-php-extensions simplexml;
+install-php-extensions soap;
+install-php-extensions sockets;
+install-php-extensions sqlite3;
+install-php-extensions tidy;
+install-php-extensions uuid;
+install-php-extensions xml;
+install-php-extensions xmlwriter;
+install-php-extensions xsl;
+install-php-extensions zip;
