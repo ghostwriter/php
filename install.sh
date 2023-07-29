f180fb42e28014c83c83302fa0a51fa07723d497
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-29 11:05:40 -0500

    Delete install.sh
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -o errexit
-set -o nounset
-
-# PHP 8.3 not supported yet
-if [ $(php -r "echo version_compare(PHP_VERSION, '8.2.999', '<');") = 1 ]; then
-    install-php-extensions imagick;
-fi
