9e5ccd9c77b2ef1d904f086247c8b40105516a56
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-12 11:32:20 -0500

    Create extensions.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ extensions.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+$phpVersion = PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;
+
+$extensions = [
+    'apcu', 'bcmath', 'bz2', 'curl', 'dom', 'ftp',
+    'gd', 'gmp', 'gnupg', 'igbinary', 'imap', 'intl', 'ldap', 'libxml',
+    'mbstring', 'memcached', 'mongodb', 'msgpack', 'odbc', 'opcache', 'pcntl',
+    'pdo', 'pdo_mysql', 'pdo_odbc', 'pdo_pgsql', 'pdo_sqlite', 'pdo_sqlsrv',
+    'readline', 'simplexml', 'soap', 'sockets', 'sqlite3', 'sqlsrv', 'tidy',
+    'uuid', 'xml', 'xmlwriter', 'xsl', 'zip',
+];
+
+$excludeExtensions = [
+    '8.3' => ['imagick'],
+    '8.4' => ['imagick', 'imap', 'pcov']
+];
+
+$requiredExtensions = array_diff($extensions, $excludeExtensions[$phpVersion] ?? []);
+
+$missingExtensions = array_filter($requiredExtensions, static fn(string $extension) => ! extension_loaded($extension));
+
+echo implode(' ', $missingExtensions);
