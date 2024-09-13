bcba66dedf69681a2cda5e607870d3a94e539b1a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 14:31:24 -0500

    Update extensions.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- extensions.php
+++ extensions.php
@@ -2,7 +2,16 @@
 
 declare(strict_types=1);
 
-$phpVersion = PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;
+$sapi = \mb_strtolower(\PHP_SAPI);
+
+$phpVariant = match (true) {
+    \str_contains($sapi, 'fpm') => 'fpm',
+    \str_contains($sapi, 'zts') => 'zts',
+    \str_contains($sapi, 'cli') => 'cli',
+    default => throw new \RuntimeException('Unknown PHP SAPI: ' . $sapi),
+};
+
+$phpVersion = \PHP_MAJOR_VERSION . '.' . \PHP_MINOR_VERSION;
 
 $extensions = [
     'apcu', 'bcmath', 'bz2', 'curl', 'dom', 'ftp',
@@ -14,17 +23,34 @@ $extensions = [
 ];
 
 $excludeExtensions = [
-    '8.3' => ['imagick'],
-    '8.4' => ['imagick', 'imap', 'pcov'],
+    '7.3' => [
+        '*' => [],
+        'cli' => [],
+        'fpm' => [],
+        'zst' => [],
+    ],
+    '8.3' => [
+        '*' => ['imagick'],
+    ],
+    '8.4' => [
+        '*' => ['imagick', 'imap', 'pcov'],
+    ],
 ];
 
-$requiredExtensions = array_diff($extensions, $excludeExtensions[$phpVersion] ?? []);
-
-$missingExtensions = array_filter(
-    $requiredExtensions,
-    static function (string $extension): bool {
-        return !extension_loaded($extension);
-    }
+$requiredExtensions = \array_diff(
+    $extensions,
+    $excludeExtensions[$phpVersion]['*'] ?? [],
+    $excludeExtensions[$phpVersion][$phpVariant] ?? []
 );
 
-echo implode(' ', $missingExtensions);
+$missingExtensions = \array_filter(
+    $requiredExtensions,
+    static fn (string $extension): bool => ! \extension_loaded($extension)
+);
+
+if ($missingExtensions === []) {
+    // All required extensions are loaded
+    $missingExtensions = ['mbstring'];
+}
+
+echo \implode(' ', $missingExtensions);
