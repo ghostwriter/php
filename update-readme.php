8d0aaefec3d0ed6b89de8ada6f28a514649b25c2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 14:31:30 -0500

    Update update-readme.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- update-readme.php
+++ update-readme.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 $versions = ['7.3', '7.4', '8.0', '8.1', '8.2', '8.3', '8.4-rc'];
 
-arsort($versions);
+\arsort($versions);
 
 $dev = '8.4-rc';
 $latest = '8.3';
 
-$variant = ['cli', 'fpm', 'zts'];
-$types = ['cli', 'fpm', 'composer'];
+$variants = ['cli', 'fpm', 'zts'];
 
 $extensions = [
     //    'database' => ['pgsql'],
@@ -24,173 +23,117 @@ $extensions = [
 
 function terminalTemplate(string $phpVersion): string
 {
-    return sprintf(
-        '### ![Terminal](resource/icons/terminal.svg) Pull & Run `PHP %s` image from the command line',
+    return \sprintf(
+        '#### ![Terminal](resource/icons/terminal.svg) Pull & Run `PHP %s` image from the command line',
         $phpVersion
-    ) . PHP_EOL;
-}
-function terminalTemplateWithType(string $phpVersion, string $type): string
-{
-    return sprintf(
-        '### ![Terminal](resource/icons/terminal.svg) Pull & Run `PHP %s` %s image from the command line',
-        $phpVersion,
-        $type
-    ) . PHP_EOL;
+    ) . \PHP_EOL;
 }
 
 function codeTemplate(string $phpVersion): string
 {
-    return sprintf('### ![Code](resource/icons/code.svg) Use `PHP %s` image in Dockerfile', $phpVersion) . PHP_EOL;
-}
-function codeTemplateWithType(string $phpVersion, string $type): string
-{
-    return sprintf(
-        '### ![Code](resource/icons/code.svg) Use `PHP %s` %s image in Dockerfile',
-        $phpVersion,
-        $type
-    ) . PHP_EOL;
+    return \sprintf('#### ![Code](resource/icons/code.svg) Use `PHP %s` image in Dockerfile', $phpVersion) . \PHP_EOL;
 }
 
-function versionTemplate(string $version, array $versions, array $types, array $extensions): string
+function versionTemplate(string $version, array $variants, array $extensions): string
 {
-    $body = sprintf('## PHP %s', $version) . PHP_EOL;
+    $body = $code = $sh = [];
 
-    $body .= PHP_EOL . terminalTemplate($version);
-    $body .= PHP_EOL . versionTemplateTerminal($version);
-    $body .= PHP_EOL . codeTemplate($version);
-    $body .= PHP_EOL . versionTemplateCode($version);
+    $code[] = \dockerFile($version);
+    $sh[] = \dockerPull($version);
+    $sh[] = \dockerRun($version);
 
-    foreach ($types as $type) {
-        $body .= PHP_EOL . versionWithTemplate($version, $type);
-
-        $body .= PHP_EOL . terminalTemplateWithType($version, $type);
-        $body .= PHP_EOL . versionWithTemplateTerminal($version, $type);
-
-        if ($type === 'composer') {
-            foreach ($extensions as $types) {
-                foreach ($types as $type) {
-                    $body .= PHP_EOL . versionWithTemplate($version, $type);
-                    $body .= PHP_EOL . terminalTemplateWithType($version, $type);
-                    $body .= PHP_EOL . versionWithTemplateTerminal($version, $type);
-                    $body .= PHP_EOL . codeTemplateWithType($version, $type);
-                    $body .= PHP_EOL . versionWithTemplateCode($version, $type);
-                }
-            }
-
-            continue;
-        }
-
-        $body .= PHP_EOL . codeTemplateWithType($version, $type);
-        $body .= PHP_EOL . versionWithTemplateCode($version, $type);
+    foreach ($variants as $variant) {
+        $code[] = \dockerFile($version, $variant);
+        $sh[] = \dockerPull($version, $variant);
+        $sh[] = \dockerRun($version, $variant);
     }
 
-    return $body;
-}
+    $body[] = \sprintf('### PHP %s', $version) . \PHP_EOL;
 
-function versionWithTemplate(string $phpVersion, string $type): string
-{
-    return sprintf('## PHP %s : %s', $phpVersion, mb_strtoupper($type)) . PHP_EOL;
-}
+    $body[] = \codeTemplate($version);
+    $body[] = \implode(\PHP_EOL, $code) . \PHP_EOL;
 
-function versionWithTemplateTerminal(string $phpVersion, string $type): string
+    $body[] = \terminalTemplate($version);
+    $body[] = \implode(\PHP_EOL, $sh) . \PHP_EOL;
+
+    return \implode(\PHP_EOL, $body);
+}
+function dockerFile(string $phpVersion, ?string $variant = null): string
 {
-    return match ($type) {
-        default =>  sprintf(
+    if ($variant === null) {
+        return \sprintf(
             <<<EOT
-                ``` sh
-                docker pull ghcr.io/ghostwriter/php:%s-%s
-                ```
-
-                ``` sh
-                docker run -it --rm -v \$PWD:/opt/app -w /opt/app ghcr.io/ghostwriter/php:%s-%s vendor/bin/phpunit
+                ```Dockerfile
+                FROM ghcr.io/ghostwriter/php:%s # %s-cli with additional development tools
                 ```
                 EOT
             ,
             $phpVersion,
-            $type,
             $phpVersion,
-            $type,
-        ) . PHP_EOL,
-        'fpm' => sprintf(
-            <<<EOT
-                ``` sh
-                docker pull ghcr.io/ghostwriter/php:%s-%s
-                ```
-                EOT
-            ,
-            $phpVersion,
-            $type,
-        ) . PHP_EOL
-    };
-}
+        );
+    }
 
-function versionWithTemplateCode(string $phpVersion, string $type): string
-{
-    return sprintf(
+    return \sprintf(
         <<<EOT
-            ``` Dockerfile
+            ```Dockerfile
             FROM ghcr.io/ghostwriter/php:%s-%s
             ```
             EOT
         ,
         $phpVersion,
-        $type,
-    ) . PHP_EOL;
+        $variant,
+    );
 }
-
-function versionTemplateTerminal(string $phpVersion): string
+function dockerRun(string $phpVersion, ?string $variant = null): string
 {
-    return sprintf(
+    return \sprintf(
         <<<EOT
-            ``` sh
-            docker pull ghcr.io/ghostwriter/php:%s
-            ```
-
-            ``` sh
-            docker run -it --rm -v \$PWD:/opt/app -w /opt/app ghcr.io/ghostwriter/php:%s vendor/bin/phpunit
+            ```sh
+            docker run -it --rm -v \$PWD:/opt/app -w /opt/app ghcr.io/ghostwriter/php:%s%s vendor/bin/phpunit
             ```
             EOT
         ,
         $phpVersion,
-        $phpVersion,
-    ) . PHP_EOL;
+        $variant ? '-' . $variant : '',
+    );
 }
-
-function versionTemplateCode(string $phpVersion): string
+function dockerPull(string $phpVersion, ?string $variant = null): string
 {
-    return sprintf(
+    return \sprintf(
         <<<EOT
-            ``` Dockerfile
-            FROM ghcr.io/ghostwriter/php:%s
+            ```sh
+            docker pull ghcr.io/ghostwriter/php:%s%s
             ```
             EOT
         ,
         $phpVersion,
-    ) . PHP_EOL;
+        $variant ? '-' . $variant : '',
+    );
 }
-
-function printREADME(array $versions, array $types, array $extensions): string
+function printREADME(array $versions, array $variants, array $extensions): string
 {
-    $body = <<<EOT
+    $body[] = <<<EOT
         # PHP for Docker [![Docker CI/CD](https://github.com/ghostwriter/php/actions/workflows/docker-build-push.yml/badge.svg)](https://github.com/ghostwriter/php/actions/workflows/docker-build-push.yml)
 
         Development and Production-ready PHP Images for Docker
 
         **Special thanks to [@mlocati](https://github.com/mlocati) for creating the fantastic [`mlocati/docker-php-extension-installer`](https://github.com/mlocati/docker-php-extension-installer) tool, which made all of this possible!**
-
-        > **Supported versions:
         EOT;
 
-    $body .= ' ' . implode(', ', $versions) . '**' . PHP_EOL;
+    $body[] = \PHP_EOL . '> **Supported versions: [`' . \implode('`, `', $versions) . '`]**' . \PHP_EOL;
 
     foreach ($versions as $version) {
-        $body .= PHP_EOL . versionTemplate($version, $versions, $types, $extensions);
+        $body[] = \versionTemplate($version, $variants, $extensions) . \PHP_EOL;
     }
 
-    return sprintf('%s', $body);
+    return \implode(\PHP_EOL, $body);
 }
 
-file_put_contents('README.md', printREADME($versions, $types, $extensions));
+$readme = \printREADME($versions, $variants, $extensions);
 
-print 'README.md updated' . PHP_EOL;
+//die($readme);
+
+\file_put_contents('README.md', $readme);
+
+print 'README.md updated' . \PHP_EOL;
 exit(0);
