6bb03645b41db7ac80a18b26ff5fd600845ab112
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 21:30:50 -0500

    Update matrix.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- matrix.php
+++ matrix.php
@@ -2,15 +2,13 @@
 
 declare(strict_types=1);
 
-$dev = '8.4-rc';
-$latest = '8.3';
-$variants = ['cli', 'fpm', 'zts'];
 $versions = ['5.4', '5.5', '5.6', '7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3', '8.4-rc'];
+
 \arsort($versions);
 
 echo \json_encode([
-    'dev' => [$dev],
-    'latest' => [$latest],
+    'dev' => ['8.4-rc'],
+    'latest' => ['8.3'],
     'version' => \array_values($versions),
-    'variant' => \array_values($variants),
+    'variant' => \array_values(['cli', 'fpm', 'zts']),
 ], \JSON_THROW_ON_ERROR);
