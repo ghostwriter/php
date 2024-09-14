f63705223a866663b85961e2ca578c0a2c63ba30
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 20:37:50 -0500

    Update matrix.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- matrix.php
+++ matrix.php
@@ -2,34 +2,15 @@
 
 declare(strict_types=1);
 
-$versions = ['5.4', '7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3', '8.4-rc'];
-
-\arsort($versions);
-
 $dev = '8.4-rc';
 $latest = '8.3';
 $variants = ['cli', 'fpm', 'zts'];
+$versions = ['5.4', '5.5', '5.6', '7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3', '8.4-rc'];
+\arsort($versions);
 
-function matrix(array $versions, array $variants, string $latest, string $dev): array
-{
-    $include = [];
-
-//    foreach ($versions as $version) {
-//        foreach ($variants as $variant) {
-//            $include[] = [
-//                'version' => $version,
-//                'variant' => $variant,
-//            ];
-//        }
-//    }
-
-    return [
-        'dev' => [$dev],
-        'latest' => [$latest],
-        'include' => \array_values($include),
-        'version' => \array_values($versions),
-        'variant' => \array_values($variants),
-    ];
-}
-
-echo \json_encode(\matrix($versions, $variants, $latest, $dev), \JSON_THROW_ON_ERROR);
+echo \json_encode([
+    'dev' => [$dev],
+    'latest' => [$latest],
+    'version' => \array_values($versions),
+    'variant' => \array_values($variants),
+], \JSON_THROW_ON_ERROR);
