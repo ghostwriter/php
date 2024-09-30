8f6604a746b555f61681e8acb611740991ec4679
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-30 12:00:01 -0500

    Update extensions.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- extensions.php
+++ extensions.php
@@ -3,11 +3,33 @@
 $sapi = \mb_strtolower(\PHP_SAPI);
 
 $debug = \getenv('RUNNER_DEBUG') !== false;
-$env = \getenv('GITHUB_CONTEXT');
-if ($debug && $env !== false) {
-    $context = \json_decode($env, true);
+if ($debug) {
+    $eventPath = \getenv('GITHUB_EVENT_PATH');
 
-    var_dump($context);
+    if ($eventPath === false) {
+        echo 'GITHUB_EVENT_PATH not found';
+        exit(1);
+    }
+
+    $event = \json_decode(\file_get_contents($eventPath), true);
+
+    if ($event === null) {
+        echo 'Failed to parse GITHUB_EVENT_PATH';
+        exit(1);
+    }
+
+    $environment = \json_decode(\getenv(), true);
+
+    if ($environment === null) {
+        echo 'Failed to parse environment';
+        exit(1);
+    }
+
+    var_dump([
+        'environment' => $environment,
+        'event' => $event,
+        'workspace' => \getenv('GITHUB_WORKSPACE'),
+    ]);
 
     exit(1);
 }
