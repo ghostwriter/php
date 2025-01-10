2e5f603172f0607fe0cd7c5d8c3b0ed8b0608bd6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-10 10:57:02 -0600

    Update debug.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- debug.php
+++ debug.php
@@ -1,28 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 if (\getenv('RUNNER_DEBUG') !== false) {
     $eventPath = \getenv('GITHUB_EVENT_PATH');
 
-    if ($eventPath === false) {
+    if (false === $eventPath) {
         echo 'GITHUB_EVENT_PATH not found';
         exit(1);
     }
 
     $event = \json_decode(\file_get_contents($eventPath), true);
 
-    if ($event === null) {
+    if (null === $event) {
         echo 'Failed to parse GITHUB_EVENT_PATH';
         exit(1);
     }
 
     $environment = \json_decode(\getenv(), true);
 
-    if ($environment === null) {
+    if (null === $environment) {
         echo 'Failed to parse environment';
         exit(1);
     }
 
-    var_dump([
+    \var_dump([
         'event' => $event,
         'environment' => $environment,
     ]);
