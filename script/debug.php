4e0c50df45356dc92d2528ad1b6c02eddc847fff
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-17 19:46:58 -0600

    Create debug.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ script/debug.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+if (\getenv('RUNNER_DEBUG') !== false) {
+    $eventPath = \getenv('GITHUB_EVENT_PATH');
+
+    if (false === $eventPath) {
+        echo 'GITHUB_EVENT_PATH not found';
+        exit(1);
+    }
+
+    $event = \json_decode(\file_get_contents($eventPath), true);
+
+    if (null === $event) {
+        echo 'Failed to parse GITHUB_EVENT_PATH';
+        exit(1);
+    }
+
+    $environment = \json_decode(\getenv(), true);
+
+    if (null === $environment) {
+        echo 'Failed to parse environment';
+        exit(1);
+    }
+
+    \var_dump([
+        'event' => $event,
+        'environment' => $environment,
+    ]);
+
+    exit(1);
+}
