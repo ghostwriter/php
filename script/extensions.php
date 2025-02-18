fc5e68ac90b2cfe7398e7fec4fc6289da6125088
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-02-17 19:47:05 -0600

    Create extensions.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ script/extensions.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+$missingExtensions = \array_filter(
+    [
+        'apcu', 'bcmath', 'bz2', 'curl', 'dom', 'ftp',
+        'gd', 'gmp', 'gnupg', 'igbinary', 'imap', 'intl', 'ldap', 'libxml',
+        'mbstring', 'memcached', 'mongodb', 'msgpack', 'odbc', 'opcache', 'pcntl',
+        'pdo', 'pdo_mysql', 'pdo_odbc', 'pdo_pgsql', 'pdo_sqlite',
+        'readline', 'simplexml', 'soap', 'sockets', 'sqlite3', 'sqlsrv', 'tidy',
+        'xml', 'xmlwriter', 'xsl', 'zip',
+    ],
+    static fn ($extension) => ! \extension_loaded($extension)
+);
+
+if ([] === $missingExtensions) {
+    // All required extensions are loaded
+    $missingExtensions = ['mbstring'];
+}
+
+echo \implode(' ', $missingExtensions);
