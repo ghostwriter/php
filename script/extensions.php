d3b20ebcbc36e8b0539731ee0cfcf7f039f085cd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-05-10 19:35:08 -0500

    Add imagick extension
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- script/extensions.php
+++ script/extensions.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $missingExtensions = \array_filter(
     [
         'apcu', 'bcmath', 'bz2', 'curl', 'dom', 'ftp',
-        'gd', 'gmp', 'gnupg', 'igbinary', 'imap', 'intl', 'ldap', 'libxml',
+        'gd', 'gmp', 'gnupg', 'igbinary', 'imagick', 'imap', 'intl', 'ldap', 'libxml',
         'mbstring', 'memcached', 'mongodb', 'msgpack', 'odbc', 'opcache', 'pcntl',
         'pdo', 'pdo_mysql', 'pdo_odbc', 'pdo_pgsql', 'pdo_sqlite',
         'readline', 'simplexml', 'soap', 'sockets', 'sqlite3', 'sqlsrv', 'tidy',
