5a72e70f8b726bd15143bf7e40f0ec5a13e0d343
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-09-26 14:45:54 -0500

    Create tools.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ script/tools.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+echo \implode(' ', [
+    'bash',
+    'ca-certificates',
+    'curl',
+    'git',
+    'github-cli',
+    'jq',
+    'make',
+    'openrc',
+    'patch',
+    'sudo',
+]);
