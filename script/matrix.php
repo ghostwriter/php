a50d3c94700beeb8475afe60b90d8cd704abf25d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-12-09 10:29:03 -0600

    Update matrix.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- script/matrix.php
+++ script/matrix.php
@@ -5,7 +5,7 @@ defined('JSON_THROW_ON_ERROR') || define('JSON_THROW_ON_ERROR', 4194304);
 //$dev = '8.6-dev';
 $dev = '8.5';
 
-$versions = array_unique(['5.4', '5.5', '5.6', '7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2', '8.3', '8.4', '8.5', $dev]);
+$versions = array_unique(['7.3', '7.4', '8.0', '8.1', '8.2', '8.3', '8.4', '8.5', $dev]);
 
 usort($versions, function ($left, $right) {
     return version_compare($right, $left);
