d8b3d2c97425c5ce3ebea8a874dbc3c750f46527
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-09-01 22:59:12 -0500

    Update matrix.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- script/matrix.php
+++ script/matrix.php
@@ -4,33 +4,34 @@ defined('JSON_THROW_ON_ERROR') || define('JSON_THROW_ON_ERROR', 4194304);
 
 $dev = '8.6';
 
-$include = [];
+$frankenphpVersions = ['8.2', '8.3', '8.4', '8.5', '8.6'];
 
-$versions = array_unique(['7.3', '7.4', '8.0', '8.1', '8.2', '8.3', '8.4', '8.5', $dev]);
+$phpVersions = ['7.3', '7.4', '8.0', '8.1', '8.2', '8.3', '8.4', '8.5', '8.6'];
 
 $variants = ['cli', 'fpm', 'zts'];
 
-foreach($variants as $variant) {
-    foreach($versions as $version) {
-        $include[] = ['variant' => $variant, 'version'=> $version, 'continueOnError' => $version === $dev];
+$include = [];
+
+foreach ($variants as $variant) {
+    foreach ($phpVersions as $phpVersion) {
+        $include[] = [
+            'variant' => $variant,
+            'version' => $phpVersion,
+            'continueOnError' => $phpVersion === $dev,
+        ];
     }
 }
 
-$frankenphpVersions = ['8.2', '8.3', '8.4', '8.5'];
-
-foreach($frankenphpVersions as $frankenphpVersion) {
-    $include[] = ['variant' => 'frankenphp', 'version'=> $frankenphpVersion, 'continueOnError' => $frankenphpVersion === $dev];
+foreach ($frankenphpVersions as $frankenphpVersion) {
+    $include[] = [
+        'variant' => 'frankenphp',
+        'version' => $frankenphpVersion,
+        'continueOnError' => $frankenphpVersion === $dev,
+    ];
 }
 
-usort($versions, function ($left, $right) {
-    return version_compare($right, $left);
-});
-
 try {
-    echo json_encode([
-        'include' => $include,
-        'exclude' => [],
-    ], JSON_THROW_ON_ERROR);
+    echo json_encode(['include' => $include], JSON_THROW_ON_ERROR);
     exit(0);
 } catch (Exception $e) {
     echo 'Error encoding JSON';
