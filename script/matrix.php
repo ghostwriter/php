646c40d32d6212e3d8a5f5d4159d10e4bce6f8ac
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-09-01 22:16:22 -0500

    Update matrix.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- script/matrix.php
+++ script/matrix.php
@@ -28,6 +28,7 @@ usort($versions, function ($left, $right) {
 
 try {
     echo json_encode([
+        'variants' => $variants,
         'include' => $include,
         'exclude' => [],
     ], JSON_THROW_ON_ERROR);
